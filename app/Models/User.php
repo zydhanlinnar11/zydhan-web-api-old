603468ab7f9bbdb5afd8a7190734c742f01f3e7f
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-01-09 19:24:47 +0700

    feat: change user primary key to uuid and remove unused column

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,25 +20,5 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
     ];
 }
