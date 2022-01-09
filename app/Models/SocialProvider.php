4c85892b6a09bdd4abe9cb93bb1b5edca4b8e933
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-01-09 12:31:14 +0700

    feat(model): add model for social provider
    Create new model with name column as fillable and automatic UUID assignment.

---
+++ app/Models/SocialProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SocialProvider extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+    /**
+     * The data type of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::creating(function ($social_provider) {
+            if (!$social_provider['id']) {
+                $social_provider['id'] = (string) \Illuminate\Support\Str::uuid();
+            }
+        });
+    }
+}
