eb77182bfaad3f90e0d1b275bdc1bb3b090ed535
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-01-09 19:53:36 +0700

    feat: resolve social provider by its name

--- app/Models/SocialProvider.php
+++ app/Models/SocialProvider.php
@@ -57,4 +57,16 @@ class SocialProvider extends Model
     {
         return $this->belongsToMany(User::class)->withTimestamps()->withPivot('social_id', 'token', 'refresh_token');
     }
+    
+    /**
+     * Retrieve the model for a bound value.
+     *
+     * @param  mixed  $value
+     * @param  string|null  $field
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
+    public function resolveRouteBinding($value, $field = null)
+    {
+        return $this->where('name', $value)->firstOrFail();
+    }
 }
