8051577973f88f6a1362f7a3ebe573a187ef2970
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-01-09 19:29:59 +0700

    feat: automatically create uuid for user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,4 +21,32 @@ class User extends Authenticatable
         'name',
         'email',
     ];
+
+    /**
+     * The data type of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::creating(function ($user) {
+            if (!$user['id']) {
+                $user['id'] = (string) \Illuminate\Support\Str::uuid();
+            }
+        });
+    }
 }
