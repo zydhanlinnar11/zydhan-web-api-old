9227e10b31b6f2bdc3f9e1b3efd7993d1ae9baeb
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-01-09 12:35:27 +0700

    feat: automatically create uuid for user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,11 +23,39 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     ];
 
     /**
-     * The attributes excluded from the model's JSON form.
+     * The attributes that should be cast.
      *
      * @var array
      */
-    protected $hidden = [
-        'password',
+    protected $casts = [
+        'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * The data type of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::creating(function ($user) {
+            if (!$user['id']) {
+                $user['id'] = (string) \Illuminate\Support\Str::uuid();
+            }
+        });
+    }
 }
